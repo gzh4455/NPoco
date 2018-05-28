57598f850eecac11f648df08313fecef26952243
gzh4455 <gzh4455@hotmail.com>
2018-05-28 22:12:57 +0800

    Init All

---
+++ Console/Model.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using NPoco;
+
+namespace Console
+{
+    public class Model
+    {
+        [TableName("Permission")]
+        [PrimaryKey("Id")]
+        [ExplicitColumns]
+        public partial class Permission
+        {
+            [Column]
+            public string PermissionId { get; set; }
+
+            [Column]
+            public string PermissionLevel { get; set; }
+
+            [Column]
+            public string Note { get; set; }
+
+            [Column]
+            public string UserId { get; set; }
+
+            [Column]
+            public int Id { get; set; }
+
+
+
+        }
+
+        [TableName("User")]
+        [PrimaryKey("UserId", AutoIncrement = false)]
+        [ExplicitColumns]
+        public partial class User
+        {
+            public User()
+            {
+                Permissions = new List<Permission>();
+            }
+            [Column]
+            public string UserId { get; set; }
+
+            [Column]
+            public string UserName { get; set; }
+
+            [Column]
+            public bool? Sex { get; set; }
+
+            [Column]
+            public string HomeAddress { get; set; }
+
+
+            [Reference(ReferenceType.Many, ColumnName = "UserId", ReferenceMemberName = "UserId")]
+            public List<Permission> Permissions { get; set; }
+        }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPoco;
+
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDatabase db = new Database("NorthWind");
+
+            //Model.User oUser = db.Query<Model.User>().Where(x => x.UserId == "001").SingleOrDefault();
+
+            ////Model.User oUser = db.SingleById<Model.User>("001");
+
+            //System.Console.WriteLine(oUser.UserId);
+            //System.Console.Write(oUser.Permissions.AsQueryable().ToList().Count);
+
+            Model.User oUser = db.Query<Model.User>().IncludeMany(p => p.Permissions).Where(x => x.UserId == "003").FirstOrDefault();
+            System.Console.WriteLine(oUser.UserId);
+            System.Console.WriteLine(oUser.Permissions.Count);
+
+
+            foreach (var permission in oUser.Permissions)
+            {
+                System.Console.WriteLine("Level:"+permission.PermissionLevel);
+            }
+
+            System.Console.WriteLine("Done");
+            System.Console.ReadKey();
+
+        }
+    }
+}
